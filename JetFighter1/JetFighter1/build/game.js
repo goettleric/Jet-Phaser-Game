72d115409e4d03c28091aa737693ba2044d64397
Eric Goettl <eric.l.goettl@gmail.com>
2017-04-05 07:24:07 -0500

    Phaser Template Addon

---
+++ JetFighter1/JetFighter1/build/game.js
@@ -0,0 +1,206 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var JetFighter;
+(function (JetFighter) {
+    var Client;
+    (function (Client) {
+        var GameEngine = (function (_super) {
+            __extends(GameEngine, _super);
+            function GameEngine() {
+                var _this = _super.call(this, 1024, 800, Phaser.AUTO, 'content', null) || this;
+                _this.state.add('Boot', Client.Boot, false);
+                _this.state.add('Preloader', Client.Preloader, false);
+                _this.state.add('MainMenu', Client.MainMenu, false);
+                _this.state.add('Level01', Client.Level01, false);
+                _this.state.start('Boot');
+                return _this;
+            }
+            return GameEngine;
+        }(Phaser.Game));
+        Client.GameEngine = GameEngine;
+    })(Client = JetFighter.Client || (JetFighter.Client = {}));
+})(JetFighter || (JetFighter = {}));
+window.onload = function () {
+    new JetFighter.Client.GameEngine();
+};
+var JetFighter;
+(function (JetFighter) {
+    var Client;
+    (function (Client) {
+        var Player = (function (_super) {
+            __extends(Player, _super);
+            function Player(game, x, y) {
+                var _this = _super.call(this, game, x, y, 'level01-sprites', 1) || this;
+                _this.anchor.setTo(0.5);
+                _this.animations.add('fly', [0, 1], 5, true);
+                game.add.existing(_this);
+                game.physics.enable(_this);
+                _this.body.collideWorldBounds = true;
+                _this.body.setCircle(20);
+                return _this;
+            }
+            Player.prototype.update = function () {
+                this.body.velocity.x = 0;
+                if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                    this.body.velocity.x = -50;
+                    this.animations.play('fly');
+                    if (this.scale.x === -1) {
+                        this.scale.x = 1;
+                    }
+                }
+                else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+                    this.body.velocity.x = 50;
+                    this.animations.play('fly');
+                    if (this.scale.x === 1) {
+                        this.scale.x = -1;
+                    }
+                }
+                else if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+                    this.body.velocity.y = -50;
+                    this.animations.play('fly');
+                    if (this.scale.y === -1) {
+                        this.scale.y = 1;
+                    }
+                }
+                else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+                    this.body.velocity.y = 50;
+                    this.animations.play('fly');
+                    if (this.scale.y === 1) {
+                        this.scale.y = -1;
+                    }
+                }
+                else {
+                    this.animations.frame = 0;
+                }
+            };
+            return Player;
+        }(Phaser.Sprite));
+        Client.Player = Player;
+    })(Client = JetFighter.Client || (JetFighter.Client = {}));
+})(JetFighter || (JetFighter = {}));
+var JetFighter;
+(function (JetFighter) {
+    var Client;
+    (function (Client) {
+        var Boot = (function (_super) {
+            __extends(Boot, _super);
+            function Boot() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            Boot.prototype.preload = function () {
+            };
+            Boot.prototype.create = function () {
+                this.stage.setBackgroundColor(0xFFFFFF);
+                this.input.maxPointers = 1;
+                this.stage.disableVisibilityChange = true;
+                if (this.game.device.desktop) {
+                    this.scale.pageAlignHorizontally = true;
+                }
+                else {
+                    this.scale.minWidth = 480;
+                    this.scale.minHeight = 260;
+                    this.scale.maxWidth = 1024;
+                    this.scale.maxHeight = 768;
+                    this.scale.forceLandscape = true;
+                    this.scale.pageAlignHorizontally = true;
+                    this.scale.refresh();
+                }
+                this.game.state.start('Preloader', true, false);
+            };
+            return Boot;
+        }(Phaser.State));
+        Client.Boot = Boot;
+    })(Client = JetFighter.Client || (JetFighter.Client = {}));
+})(JetFighter || (JetFighter = {}));
+var JetFighter;
+(function (JetFighter) {
+    var Client;
+    (function (Client) {
+        var Level01 = (function (_super) {
+            __extends(Level01, _super);
+            function Level01() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            Level01.prototype.create = function () {
+                this.physics.startSystem(Phaser.Physics.ARCADE);
+                this.background = this.add.sprite(0, 0, 'level01-sprites', 'background');
+                this.player = new Client.Player(this.game, this.world.centerX, this.world.centerX);
+                this.player.anchor.setTo(0, 5);
+                this.game.debug.text("Use Right and Left arrow keys to move the bat", 0, this.world.height, "red");
+            };
+            return Level01;
+        }(Phaser.State));
+        Client.Level01 = Level01;
+    })(Client = JetFighter.Client || (JetFighter.Client = {}));
+})(JetFighter || (JetFighter = {}));
+var JetFighter;
+(function (JetFighter) {
+    var Client;
+    (function (Client) {
+        var MainMenu = (function (_super) {
+            __extends(MainMenu, _super);
+            function MainMenu() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            MainMenu.prototype.create = function () {
+                this.background = this.add.sprite(0, 0, 'titlepage');
+                this.background.alpha = 0;
+                this.logo = this.add.sprite(this.world.centerX, -300, 'logo');
+                this.logo.anchor.setTo(0.5);
+                this.add.tween(this.background).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true);
+                this.add.tween(this.logo).to({ y: 400 }, 2000, Phaser.Easing.Elastic.Out, true, 500);
+                this.game.debug.text("Click to start the game", this.world.width / 2.9, this.world.height - 100, "red");
+                this.input.onDown.addOnce(this.fadeOut, this);
+            };
+            MainMenu.prototype.fadeOut = function () {
+                this.add.audio('click', 1, false).play();
+                this.add.tween(this.background).to({ alpha: 0 }, 4000, Phaser.Easing.Linear.None, true);
+                var tween = this.add.tween(this.logo).to({ y: 1200 }, 4000, Phaser.Easing.Linear.None, true);
+                tween.onComplete.add(this.startGame, this);
+            };
+            MainMenu.prototype.startGame = function () {
+                this.game.state.start('Level01', true, false);
+            };
+            return MainMenu;
+        }(Phaser.State));
+        Client.MainMenu = MainMenu;
+    })(Client = JetFighter.Client || (JetFighter.Client = {}));
+})(JetFighter || (JetFighter = {}));
+var JetFighter;
+(function (JetFighter) {
+    var Client;
+    (function (Client) {
+        var Preloader = (function (_super) {
+            __extends(Preloader, _super);
+            function Preloader() {
+                return _super !== null && _super.apply(this, arguments) || this;
+            }
+            Preloader.prototype.preload = function () {
+                this.loaderText = this.game.add.text(this.world.centerX, 200, "Loading...", { font: "18px Arial", fill: "#A9A91111", align: "center" });
+                this.loaderText.anchor.setTo(0.5);
+                this.load.image('titlepage', './assets/ui/titlePage.png');
+                this.load.image('logo', './assets/ui/gameLogo.png');
+                this.load.audio('click', './assets/sounds/aircraft009.mp3', true);
+                this.load.atlasJSONHash('level01-sprites', './assets/sprites/level01-sprites.png', './assets/sprites/level01-sprites.json');
+            };
+            Preloader.prototype.create = function () {
+                var tween = this.add.tween(this.loaderText).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+                tween.onComplete.add(this.startMainMenu, this);
+            };
+            Preloader.prototype.startMainMenu = function () {
+                this.game.state.start('MainMenu', true, false);
+            };
+            return Preloader;
+        }(Phaser.State));
+        Client.Preloader = Preloader;
+    })(Client = JetFighter.Client || (JetFighter.Client = {}));
+})(JetFighter || (JetFighter = {}));
+//# sourceMappingURL=game.js.map
---
+++ JetFighter1/JetFighter1/src/gameStates/mainMenu.ts
@@ -0,0 +1,37 @@
+﻿module JetFighter.Client {
+
+    export class MainMenu extends Phaser.State {
+        
+        background: Phaser.Sprite;
+        logo: Phaser.Sprite;
+
+        create() {
+            this.background = this.add.sprite(0, 0, 'titlepage');
+            this.background.alpha = 0;
+
+            this.logo = this.add.sprite(this.world.centerX, -300, 'logo');
+            this.logo.anchor.setTo(0.5);
+
+            this.add.tween(this.background).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true);
+            this.add.tween(this.logo).to({ y: 400 }, 2000, Phaser.Easing.Elastic.Out, true, 500);
+
+            this.game.debug.text("Click to start the game", this.world.width/2.9, this.world.height -100, "red");
+
+            this.input.onDown.addOnce(this.fadeOut, this);
+        }
+
+        fadeOut() {
+            this.add.audio('click', 1, false).play();
+            
+            this.add.tween(this.background).to({ alpha: 0 }, 4000, Phaser.Easing.Linear.None, true);
+            var tween = this.add.tween(this.logo).to({ y: 1200 }, 4000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startGame, this);
+        }
+
+        startGame() {
+            this.game.state.start('Level01', true, false);
+        }
+
+    }
+
+}
